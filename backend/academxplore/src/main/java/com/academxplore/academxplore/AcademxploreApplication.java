a59e40a5004b6a8d75bf23dd606768b71f45aa6f
gabrielaraujog9 <gabrielaraujogoncalves14@gmail.com>
2023-08-28 19:41:57 -0300

    criação do projeto java

---
+++ backend/academxplore/src/main/java/com/academxplore/academxplore/AcademxploreApplication.java
@@ -0,0 +1,13 @@
+package com.academxplore.academxplore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademxploreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademxploreApplication.class, args);
+	}
+
+}
